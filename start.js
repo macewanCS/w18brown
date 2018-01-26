954c09330c36f8242a073eb4858c99e17905ef74
reallynewbie <bruceleung@shaw.ca>
2018-01-26 09:56:45 -0700

    Adding in Express.js and updating webpage + start.js
    Includes Express which we may use later, and updating the html/start.js
    to handle posting form info.

--- start.js
+++ start.js
@@ -4,17 +4,40 @@ var path = require('path')
 
 // createServer() creates the http server object
 http.createServer(function (req, res) {
+	if (req.method.toLowerCase() == 'get') {
+		getWebpage(res);
+	}
+	else if (req.method.toLowerCase() == 'post') {
+		getLoginForm(req, res);
+	}
 
-	fs.readFile('index.html', function(err, data) {
+
+}).listen(8080); // needs to be updated for web
+
+function getWebpage(res) {
+	fs.readFile('index.html', function (err, data) {
 
 		// writeHead ( status code, { response header });
 		// !!!! text/html   text/plain   tells the browser how to show it.
-		res.writeHead(200, {'Content-Type': 'text/html'});
-	
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+
 		// writes data from html file
 		res.write(data);
 		res.end();
-	
+
 	});
+	//Doesn't work all the time due to asyncronous execution.  res can be ended before the write happens.
+	if (req.url.indexOf('.css') != -1) { //req.url has the pathname, check if it contains '.css'
+		fs.readFile(__dirname + '/Stylesheets/login.css', function (err1, data1) {
+			if (err1) console.log(err1);
+			res.writeHead(200, { 'Content-Type': 'text/css' });
+			res.write(data1);
+			res.end();
+		});
+
+	}
+}
+
+function getLoginForm(req, res) {
 	
-}).listen(8080); // needs to be updated for web
+}

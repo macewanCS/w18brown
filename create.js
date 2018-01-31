2b8419ba6137a1e731514c3797c01045922bb19a
Peter Bordush <pbordush@gmail.com>
2018-01-30 21:09:50 -0700

    Add database code

---
+++ create.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "brown"
+});
+
+con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+	  con.query("CREATE DATABASE carraway", function (err, result) {
+		      if (err) throw err;
+		      console.log("Database created");
+		    });
+});

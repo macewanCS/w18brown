89ed3e76fa797e44c9266d23f03c1c16ac56f844
zhaom5 <zhaom5@mymacewan.ca>
2018-01-31 11:30:58 -0700

    Update admin page with account management

---
+++ scripts/account.js
@@ -0,0 +1,9 @@
+function addInput(divName){
+	if(counter == limit) {
+		alert("You have reached the limit of adding" + counter + " inputs");
+	}
+	else {
+		var newdiv = document.createElement('div');
+		
+	}
+}

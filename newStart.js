f66c1a8d3b2e96b346925e41d75bbf95a8607da3
reallynewbie <bruceleung@shaw.ca>
2018-01-30 19:45:26 -0700

    Update to ExpressJS
    Able to support HTML/CSS for both the login page and the admin page.

---
+++ newStart.js
@@ -0,0 +1,32 @@
+const port = 8080;
+const path = __dirname + "/Public/";
+
+const express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+/*-------------*/
+
+app.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+})
+app.get("/admin", function (req, res) {
+    res.sendFile(path + "admin.html");
+})
+app.get("/Stylesheets/login.css", function (req, res) {
+    res.sendFile(path + "Stylesheets/login.css");
+})
+app.get("/Stylesheets/admin.css", function (req, res) {
+    res.sendFile(path + "Stylesheets/admin.css");
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/login', function (req, res) {
+    res.send("Username: " + req.body.name);
+})
+
+app.listen(port, function() {
+    console.log("Listening on 8080");
+});
+  

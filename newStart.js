aebef1455a76601821d60ecc3a90b435824210cf
Peter Bordush <pbordush@gmail.com>
2018-01-31 19:33:18 -0700

    Add username/password check

--- newStart.js
+++ newStart.js
@@ -1,3 +1,5 @@
+var mysql = require('mysql');
+
 const port = 8080;
 const path = __dirname + "/Public/";
 
@@ -27,9 +29,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //When posted information to "/login", do this.
 app.post('/login', function (req, res) {
     res.send("Username: " + req.body.username + "\nPassword: " + req.body.password);
+    var type = checkName(req.body.username, req.body.password);
+    //I called a function here to open the propr page based on type but turns out I dont know how
 })
 
 app.listen(port, function() {
     console.log("Listening on 8080");
 });
-  
+
+//this function checks whether a username/password combo is in the database.
+//Return: account type (string)
+function checkName(name, password){
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "brown",
+          database: "carraway"
+    });
+      
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "SELECT * FROM family WHERE familyID = ? and password = ?";
+        con.query(sql, [name, password], function (err, result, fields) {
+          if (err) throw err;
+          if (result.length === 0){
+              return "blank";
+          }
+          else{
+                return result[0].type;
+          }
+      });
+    });
+}

1c4c75e405bc8b7b88bed97fe7eb02373657c998
reallynewbie <bruceleung@shaw.ca>
2018-02-06 16:01:55 -0700

    Add User dashboard and cleanup javascript for UI.

---
+++ Public/Scripts/ui.js
@@ -0,0 +1,12 @@
+//AJAX to pull up pages while in the menu
+function moveToPage(page) {
+    var xhttp = new XMLHttpRequest();
+    console.log("Test");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('mainArea').innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/" + page, true);
+    xhttp.send();
+}

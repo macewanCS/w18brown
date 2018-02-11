359696ff6a4828498f5cd7c98ff1b5b12e6a6471
reallynewbie <bruceleung@shaw.ca>
2018-02-11 12:17:46 -0700

    Minor refactor, remove test code

--- Public/Scripts/ui.js
+++ Public/Scripts/ui.js
@@ -1,8 +1,6 @@
 //AJAX to pull up pages while in the menu
-function moveToPage(page) {
-    console.log("This is run");
+function moveToPage(page) {    
     var xhttp = new XMLHttpRequest();
-    console.log("Test");
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById('mainArea').innerHTML = this.responseText;

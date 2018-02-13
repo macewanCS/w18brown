f97895a78999a7a9c0a28638718aefa0046c7369
Peter Bordush <pbordush@gmail.com>
2018-02-12 20:33:03 -0700

    set Settings complete and tested

--- functions.js
+++ functions.js
@@ -69,6 +69,35 @@ async function getSettings(){
 	});
 }
 
+/**
+ * 
+ * @param {*} input pass in the format as follows: var input = ["08:45", "12:00", "11:50", "13:00", "12:50", "15:45", "2018/09/05", "5:00"];
+ * Currently no return value but we will have error checking on this down the road.
+ */
+async function setSettings(input){
+    return new Promise(function(fulfill, reject){
+		var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "brown",
+              database: "carraway"
+        });
+		
+
+		con.connect(function(err) {
+            if (err) throw err;
+            var date;
+            // ? is like %s in C. 
+            var sql = "UPDATE settings SET block1_start = ?, block1_end = ?, block2_start = ?, block2_end = ?, block3_start = ?, block3_end = ?, year_start = ?, weekly_requirements = ?";
+                            // this array gives order. name is the first ?, password is the 2nd ?
+            con.query(sql, (input), function (err, result, fields) {
+              if (err) throw err;
+              console.log("settings updated");
+          });
+        });
+    })
+}
+
 
 
 // -- Test isEmptyString

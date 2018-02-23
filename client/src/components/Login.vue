76f3f5fb1c6936644a8401a05f1ef4ef6791715c
reallynewbie <bruceleung@shaw.ca>
2018-02-23 11:48:44 -0700

    Remove MDL CSS Classes

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -5,33 +5,25 @@ https://www.youtube.com/watch?v=Fa4cRMaTDUI -->
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Caraway Login">
-
         <h1>Caraway Volunteer Log-In Page</h1>
-
- <div class="FormArea">
-    <div class="UserInput">
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="UsernameField">
-            <!-- added v-models for linking to script, added placeholders -->
-            <input name="username" class="mdl-textfield__input" type="text" id="username" placeholder="Login" v-model="username">
-          <!-- Dont need this  <label class="mdl-textfield__label" for="username">Username</label>-->
-                    
+        <div class="FormArea">
+            <div class="UserInput">
+                <div id="UsernameField">
+                    <!-- added v-models for linking to script, added placeholders -->
+                    <input name="username" type="text" id="username" placeholder="Login" v-model="username">                     
+                </div>
+                  <br>  
+                <div id="PasswordField">
+                    <input name="password" type="password" id="password" placeholder="Password" v-model="password">
+                </div>
+            </div>
+            <div class="Buttons">
+                <button type="button" id="forgotPassword" >Forgot your Password?</button>
+                <button type="submit" id="login" @click="login"> <!-- calls the login method below in scripts-->
+                  Login
+                </button>
+            </div>
         </div>
-          <br>  
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="PasswordField">
-            <input name="password" class="mdl-textfield__input" type="password" id="password" placeholder="Password" v-model="password">
-          <!-- Dont need this <label class="mdl-textfield__label" for="password">Password</label>-->
-        </div>
-    </div>
-    <div class="Buttons">
-        <button type="button" id="forgotPassword" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Forgot your Password?</button>
-        <button type="submit" id="login" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          @click="login"> <!-- calls the login method below in scripts-->
-          Login    
-        </button>
-
-    </div>
-</div>
-
       </panel>
     </v-flex>
   </v-layout>
@@ -41,27 +33,26 @@ https://www.youtube.com/watch?v=Fa4cRMaTDUI -->
 
 <script>
 // ** script is the controller **
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from "@/services/AuthenticationService";
 
 export default {
-  data () {
+  data() {
     return {
-      username: '', // ** to update
-      password: '', // ** to update
+      username: "", // ** to update
+      password: "", // ** to update
       error: null // ** to update
-    }
+    };
   },
   methods: {
-    async login () {
-      console.log('login button was clicked')
+    async login() {
+      console.log("login button was clicked");
       try {
         const response = await AuthenticationService.login({
           username: this.username,
           password: this.password
-        })
+        });
 
-        
-        console.log(response.data)
+        console.log(response.data);
         /*
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
@@ -70,65 +61,64 @@ export default {
         })
         */
       } catch (error) {
-        this.error = error.response.data.error
+        this.error = error.response.data.error;
       }
     }
   }
-}
-
+};
 </script>
 
 <style scoped>
 body,
-    html {
-        height: 100%;
-        width: 100%;
-        margin: auto;
-        background-repeat: no-repeat;
-        background-color: #EEEEEE;
-        font-family: Lato;
-    }
+html {
+  height: 100%;
+  width: 100%;
+  margin: auto;
+  background-repeat: no-repeat;
+  background-color: #eeeeee;
+  font-family: Lato;
+}
 
-    form {
-        width: 30%;
-        background-color: #FFFFFF;
+form {
+  width: 30%;
+  background-color: #ffffff;
 
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 
-        position: absolute;
-        height: 300px;
+  position: absolute;
+  height: 300px;
 
-        padding: 50px;
-        margin: auto;
-        text-align: center;
-        /*border-style: solid;*/
-        box-shadow: 8px 8px #BDBDBD;
-    }
+  padding: 50px;
+  margin: auto;
+  text-align: center;
+  /*border-style: solid;*/
+  box-shadow: 8px 8px #bdbdbd;
+}
 
-    input {
-        width: 200px;
-        height: 25px;
-    }
+input {
+  width: 200px;
+  height: 25px;
+}
 
-    button {
-        margin-left: 1vw;
-    }
+button {
+  margin-left: 1vw;
+}
 
-    .UserInput {
-        margin-bottom: 10px;
-    }
+.UserInput {
+  margin-bottom: 10px;
+}
 
-    .Buttons {
-        text-align: center;
-    }
+.Buttons {
+  text-align: center;
+}
 
-    h1 {
-        text-transform: uppercase;
-        letter-spacing: 3px;
-        font-size: 20pt;
-        color: #0288D1;
-    }
+h1 {
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 20pt;
+  color: #0288d1;
+}
 </style>

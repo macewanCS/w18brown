c107d778f6b87fd805145a38c449360c076fdcde
Terry Youngman <terryyoungman@Terrys-MBP.hitronhub.home>
2018-02-22 23:16:49 -0700

    add nodemon, server/client vue files

---
+++ client/src/services/AuthenticationService.js
@@ -0,0 +1,17 @@
+// ** hits the endpoints. **
+
+import Api from '@/services/Api' 
+
+// this file exports this object that has a login method.
+export default {
+    login (logInfo) {
+        return Api().post('login', logInfo)
+
+    }
+}
+
+// calling the above function in another file:
+/* AuthenticationService.login({
+    account: 'some text',
+    password: 'some text'
+}) */

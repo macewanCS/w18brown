bc0adafc5e83ecfb5830e9717694f38f12469203
Terry Youngman <terryyoungman@Terrys-MacBook-Pro.local>
2018-03-03 15:55:12 -0700

    fixed login and added routing and components for all login types

---
+++ client/src/components/Family.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-content>
+        <nav-drawer />
+          <h1>Insert Family HTML Here</h1>
+        <router-view />
+    </v-content>
+</template>
+
+<script>
+import NavDrawer from "@/components/NavDrawer.vue";
+export default {
+  name: "app",
+  components: {
+    NavDrawer
+  }
+};
+</script>
+
+<style>
+
+</style>

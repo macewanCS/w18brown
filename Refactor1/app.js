8005863e3f904d0f753badc26d8a8d7e2145d2fb
reallynewbie <bruceleung@shaw.ca>
2018-03-04 12:35:27 -0700

    Remove Refactor1

--- Refactor1/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Router = require("./router");
-
-let app = express();
-Router.init(app);
-
-app.listen("8080", () => {
-    console.log("Listening on 8080");
-});

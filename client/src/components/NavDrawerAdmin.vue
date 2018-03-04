b0ebcb8a1bab8b8edccca5ca90d8f6de058dbf66
reallynewbie <bruceleung@shaw.ca>
2018-03-04 15:38:41 -0700

    Merge Admin NavDrawer

--- client/src/components/NavDrawerAdmin.vue
+++ client/src/components/NavDrawerAdmin.vue
@@ -11,18 +11,40 @@
         </v-toolbar>
         <v-divider />
         <v-list fluid>
-            <v-list-tile :to="{name: 'admin'}">
+
+            <v-list-tile :to="{name: 'dashboardadmin'}">
                 <!--looks to router/index.js for admin and loads components-->
                 <v-list-tile-content>
                     <v-list-tile-title>Dashboard</v-list-tile-title>
                 </v-list-tile-content>
             </v-list-tile>
 
-            <v-list-tile :to="{name: 'rooms'}">
+            <v-divider />
+
+            <v-list-tile>
                 <v-list-tile-content>
-                    <v-list-tile-title>Room Management</v-list-tile-title>
+                    <v-list-tile-title>Events</v-list-tile-title>
                 </v-list-tile-content>
             </v-list-tile>
+            
+            <v-list-tile :to="{name: 'underconstruction'}">
+             
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Holidays</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list-tile>
+
+           <v-list-tile :to="{name: 'underconstruction'}">
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Field Trips</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list-tile>
+
+            <v-divider />
 
             <v-list-tile :to="{name: 'accounts'}">
                 <v-list-tile-content>
@@ -30,10 +52,44 @@
                 </v-list-tile-content>
             </v-list-tile>
 
-            <v-list-tile :to="{name: 'settings'}">
+            <v-list-tile :to="{name: 'underconstruction'}">
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Family</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list-tile>
+
+            <v-list-tile :to="{name: 'underconstruction'}">
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Staff</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list-tile>
+
+            <v-divider />
+
+            <v-list-tile>
                 <v-list-tile-content>
                     <v-list-tile-title>Settings</v-list-tile-title>
                 </v-list-tile-content>
+            </v-list-tile>
+            
+            <v-list-tile :to="{name: 'rooms'}">
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Room Management</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list-tile>
+
+            <v-list-tile :to="{name: 'settings'}">
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Schedule Settings</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
             </v-list-tile>
 
             <v-divider />

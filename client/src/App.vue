596329a262c6d5a7e1742ef0cf3e64cd3677805e
reallynewbie <bruceleung@shaw.ca>
2018-03-08 23:48:15 -0700

    Reserve Layout Complete with prep for data-binding

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,11 +12,14 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+html {
+    overflow:auto;
 }
 </style>
--- client/src/components/Reserve.vue
+++ client/src/components/Reserve.vue
@@ -1,51 +1,42 @@
 <template>
-  <v-tabs fixed-tabs dark>
-    <v-tab key="tab1">
-      Room Reservations
-    </v-tab>
-    <v-tab key="tab2">
-      Field Trip Reservations
-    </v-tab>
 
-    <v-tabs-items>
-      <v-tab-item key="tab1">
         <!-- Begin Calendar -->
-        <v-container id="calendar" grid-list-sm text-xs-center>
+        <v-container id="calendar" grid-list-md text-xs-center >
           <!-- Row 1: Header -->
           <v-layout row class="cal-header">
-            <v-flex md1>
+            <v-flex md1 class="cal-times">
 
             </v-flex>
             <v-flex md3>
-              <v-card color="blue-grey" dark tile flat class="cal-day">
+              <v-card color="blue-grey" dark>
                 <v-card-text>
                   Monday
                 </v-card-text>
               </v-card>
             </v-flex>
             <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+              <v-card color="blue-grey" dark>
                 <v-card-text>
                   Tuesday
                 </v-card-text>
               </v-card>
             </v-flex>
             <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+              <v-card color="blue-grey" dark>
                 <v-card-text>
                   Wednesday
                 </v-card-text>
               </v-card>
             </v-flex>
             <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+              <v-card color="blue-grey" dark>
                 <v-card-text>
                   Thursday
                 </v-card-text>
               </v-card>
             </v-flex>
             <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+              <v-card color="blue-grey" dark>
                 <v-card-text>
                   Friday
                 </v-card-text>
@@ -55,44 +46,58 @@
           <v-divider />
           <!-- Row 2: Block 1 -->
           <v-layout row class="cal-block">
-            <v-flex md1>
-              <v-card color="blue-grey" dark>
-                <v-card-text>
+            <v-flex md1 class="cal-times">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text class="block-time">
                   Block 1
-                  <br> Insert Time
+                  <br>Insert Time
                 </v-card-text>
               </v-card>
             </v-flex>
-            <v-flex md3>
-              <v-card color="blue-grey" dark>
-                <v-card-text>
-                  Monday
-                </v-card-text>
+            <v-flex xs3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-layout row>
+                  <v-flex class="test">
+                    <v-card hover wrap v-for="fac in MonB1" :key="fac" v-bind:style="{height: fac.height}" class="facilitator">
+                      {{fac.name}} FFFFFFFFFFFFF
+                    </v-card>
+                  </v-flex>
+                  <v-flex>
+                    <v-card hover v-for="fac in MonB2" :key="fac" v-bind:style="{height: fac.height}" class="facilitator">
+                      {{fac.name}}
+                    </v-card>
+                  </v-flex>
+                  <v-flex>
+                    <v-card hover v-for="fac in MonB3" :key="fac" v-bind:style="{height: fac.height}" class="facilitator">
+                      {{fac.name}}
+                    </v-card>
+                  </v-flex>
+                </v-layout>
               </v-card>
             </v-flex>
-            <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+            <v-flex xs3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
                 <v-card-text>
                   Tuesday
                 </v-card-text>
               </v-card>
             </v-flex>
-            <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+            <v-flex xs3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
                 <v-card-text>
                   Wednesday
                 </v-card-text>
               </v-card>
             </v-flex>
-            <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+            <v-flex xs3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
                 <v-card-text>
                   Thursday
                 </v-card-text>
               </v-card>
             </v-flex>
-            <v-flex md3>
-              <v-card color="blue-grey" dark tile flat>
+            <v-flex xs3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
                 <v-card-text>
                   Friday
                 </v-card-text>
@@ -101,72 +106,116 @@
           </v-layout>
           <!-- Row 3: Block 2 -->
           <v-layout row class="cal-block">
-            <v-flex md1>
-              <v-card color="blue-grey" dark>
-                <v-card-text>
+            <v-flex md1 class="cal-times">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text class="block-time">
                   Block 2
                   <br> Insert Time
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Monday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Tuesday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Wednesday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Thursday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Friday
                 </v-card-text>
               </v-card>
             </v-flex>
           </v-layout>
           <!-- Row 4: Block 3 -->
           <v-layout row class="cal-block">
-            <v-flex md1>
-              <v-card color="blue-grey" dark>
-                <v-card-text>
+            <v-flex md1 class="cal-times">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text class="block-time">
                   Block 3
                   <br> Insert Time
                 </v-card-text>
               </v-card>
             </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Monday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Tuesday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Wednesday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Thursday
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex md3 class="cal-block-col">
+              <v-card color="blue-grey" dark class="cal-block-day">
+                <v-card-text>
+                  Friday
+                </v-card-text>
+              </v-card>
+            </v-flex>
           </v-layout>
-          <!-- <div class="week">
-            <div class="day">
-              
-            </div>
-            <div class="day">
-              Monday
-            </div>
-            <div class="day">
-              Tuesday
-            </div>
-            <div class="day">
-              Wednesday
-            </div>
-            <div class="day">
-              Thursday
-            </div>
-            <div class="day">
-              Friday
-            </div>
-          </div> -->
         </v-container>
         <!-- End Calendar -->
-      </v-tab-item>
-      <v-tab-item key="tab2">
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
-            <div id="box">
-
-              <body>
-                <h1>Reserve Facilitation Hours</h1>
-                <div>
-                </div>
-              </body>
-            </div>
-          </v-layout>
-        </v-container>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-tabs>
 
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      MonB1: [
+        { name: "test", height: "100px" },
+        { name: "test1", height: "100px" }
+      ],
+      MonB2: [
+        { name: "Free", height: "225px" }
+      ],
+      MonB3: [
+        { name: "Free", height: "225px" }
+      ]
+    };
   },
   components: {}
 };
@@ -191,13 +240,39 @@ h1 {
 }
 #calendar {
   background-color: red;
-}
-.week {
-  display: grid;
-  grid-template-columns: auto auto auto auto auto auto;
+  min-width: 1000px; 
+  max-width: 1000px;
 }
 .cal-block {
-  height: 225px;
+  height: 300px;
+  max-height: 300px;
+  min-height: 300px;
   background-color: purple;
+}
+.cal-block-col {
+  background-color: pink;
+}
+.cal-block-day {
+  min-height: 100%;
+  max-width: 300px;
+  color: red;
+  padding: 5px;
+}
+.cal-times {
+  min-width: 80px;
+  max-width: 80px;
+}
+.block-time {
+  text-align: center;
+  padding-top: 100%;
+  padding-bottom: 100%;
+}
+.facilitator {
+  margin-top: 2px;
+  width: 50px;
+  max-width: 50px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  flex: 10 0 auto;
 }
 </style>
